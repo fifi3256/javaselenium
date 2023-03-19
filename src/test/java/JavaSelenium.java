9ee10ca45b126cb6286b6e138f2cb4dd252dcea8
Chris <asn492061@gmail.com>
2023-03-19 06:16:06 -0700

    add comments..
    -- Added several comments to test for easy understanding

--- src/test/java/JavaSelenium.java
+++ src/test/java/JavaSelenium.java
@@ -9,6 +9,8 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class JavaSelenium {
+
+    // Usage of TestNG annotation to run test
     @Test
     public void WebDemo() {
 
@@ -26,18 +28,19 @@ public class JavaSelenium {
         // Go to url
         driver.get("https://www.webstaurantstore.com/");
 
-        // Implicits times to waits until table elements to load on page
+        // WebDriver polls the DOM for a certain duration when trying to find any element
         driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
 
-        // Type in the word "stainless work table" in the input field box
+        // Clear and type in the word "stainless work table" in the input field box
         driver.findElement(By.id("searchval")).clear();
         driver.findElement(By.id("searchval")).sendKeys("stainless work table");
 
+        // WebDriver polls the DOM for a certain duration when trying to find any element
+        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
+
         // Tabs to the search button and press Enter key
         driver.findElement(By.id("searchval")).sendKeys(Keys.TAB);
-        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
         driver.findElement(By.id("searchval")).sendKeys(Keys.TAB);
-        driver.manage().timeouts().implicitlyWait(500, TimeUnit.MILLISECONDS);
         driver.findElement(By.id("searchval")).sendKeys(Keys.ENTER);
 
         // Initializes a new instance of the WebDriverWait class
@@ -51,39 +54,49 @@ public class JavaSelenium {
             System.out.println(element.getText());
         }
 
-        // Implicits times to waits until table elements to load on page
+        // WebDriver polls the DOM for a certain duration when trying to find any element
         driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
 
         // Create webElement in Selenium is essentially an HTML element on a website
         WebElement addToCartLastProduct = driver.findElement(By.xpath("(//a[contains(text(),'Table')])[last()-1]"));
 
+        // WebDriver polls the DOM for a certain duration when trying to find any element
         driver.manage().timeouts().implicitlyWait(2000, TimeUnit.MILLISECONDS);
 
-        // Click on the element as create on the page
+        // Click the last product to Add to Cart
         addToCartLastProduct.click();
 
+        // WebDriver polls the DOM for a certain duration when trying to find any element
         driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
 
+        // Create webElement in Selenium is essentially an HTML element on a website
         WebElement addToCart = driver.findElement(By.xpath("(//input[@value='Add to Cart'])[1]"));
 
+        // Click Add to Cart button
         addToCart.click();
 
+        // Create webElement in Selenium is essentially an HTML element on a website
         WebElement viewCart = driver.findElement(By.xpath("//button[contains(text(), 'View Cart')]"));
 
+        // WebDriver polls the DOM for a certain duration when trying to find any element
         driver.manage().timeouts().pageLoadTimeout(15000, TimeUnit.MILLISECONDS);
 
         // JavaScriptExecutor is an interface that helps to execute JavaScript through Selenium Webdriver
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", viewCart);
 
-
+        // Wait for an element to be visible
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(text(), 'View Cart')]")));
 
-        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
+        // WebDriver polls the DOM for a certain duration when trying to find any element
+        driver.manage().timeouts().implicitlyWait(3000    , TimeUnit.MILLISECONDS);
 
+        // Click View Cart button
         viewCart.click();
 
+        // WebDriver polls the DOM for a certain duration when trying to find any element
         driver.manage().timeouts().implicitlyWait(3000, TimeUnit.MILLISECONDS);
 
+        // WebDriver polls the DOM for a certain duration when trying to find any element
         WebElement emptyCart = driver.findElement(By.xpath("//button[contains(text(), 'Empty Cart')]"));
 
         // Assertion equals text string same as element on page
@@ -92,14 +105,22 @@ public class JavaSelenium {
         // JavaScriptExecutor is an interface that helps to execute JavaScript through Selenium Webdriver
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", emptyCart);
 
+        // WebDriver polls the DOM for a certain duration when trying to find any element
         driver.manage().timeouts().pageLoadTimeout(2000, TimeUnit.MILLISECONDS);
+
+        // Click empty cart button
         emptyCart.click();
 
+        // WebDriver polls the DOM for a certain duration when trying to find any element
         WebElement emptyCartConfirmation = driver.findElement(By.xpath("(//button[contains(.,'Cart')])[2]"));
 
+        // JavaScriptExecutor is an interface that helps to execute JavaScript through Selenium Webdriver
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", emptyCartConfirmation);
 
+        // WebDriver polls the DOM for a certain duration when trying to find any element
         driver.manage().timeouts().pageLoadTimeout(2000, TimeUnit.MILLISECONDS);
+
+        // Click empty card confirmation button
         emptyCartConfirmation.click();
 
         // Close the driver browser page
